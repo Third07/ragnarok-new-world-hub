6e3a6a821d26ec8f2acf538ccacb3e7057a6c269
Third07 <thethird0716@gmail.com>
2026-08-06 06:25:01 +0800

    Add Guild League guide route

---
+++ app/guides/guild-league/page.tsx
@@ -0,0 +1,8 @@
+import SourceGuidePage, { buildGuideMetadata } from "../SourceGuidePage";
+import { guide } from "../source-guide-data/guild-league";
+
+export const metadata = buildGuideMetadata(guide);
+
+export default function Page() {
+  return <SourceGuidePage guide={guide} />;
+}
